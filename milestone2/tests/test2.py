7c06040ddf9d0698b2a890ad67857723ae273cdb
Yash Gupta <yashbg7@gmail.com>
2024-03-27 16:58:22 +0530

    Add milestone2/

---
+++ milestone2/tests/test2.py
@@ -0,0 +1,48 @@
+data: list[float] = [-2.3, 3.14, 0.9, 11, -9.1]
+
+
+def compute_min() -> float:
+  min_value: float = None
+  i: int = 0
+  for i in range(len(data)):
+    if not min_value:
+      min_value = data[i]
+    elif data[i] < min_value:
+      min_value = data[i]
+  return min_value
+
+
+def compute_max() -> float:
+  max_value: float = None
+  i: int = 0
+  for i in range(len(data)):
+    if not max_value:
+      max_value = data[i]
+    elif data[i] > max_value:
+      max_value = data[i]
+  return max_value
+
+
+def compute_avg() -> float:
+  avg_value: float = None
+  sum: int = 0
+  i: int = 0
+  for i in range(len(data)):
+    sum += data[i]
+  return sum / len(data)
+
+
+def main():
+  min_value: float = compute_min()
+  print("Minimum value: ")
+  print(min_value)
+  max_value: float = compute_max()
+  print("Maximum value: ")
+  print(max_value)
+  avg_value: float = compute_avg()
+  print("Average value: ")
+  print(avg_value)
+
+
+if __name__ == "__main__":
+  main()
