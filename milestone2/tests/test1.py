2a047ff329c0814e52974e1160cfc5117c4fcc59
Yash Gupta <yashbg7@gmail.com>
2024-03-29 03:55:01 +0530

    Update test1.py

--- milestone2/tests/test1.py
+++ milestone2/tests/test1.py
@@ -2,6 +2,7 @@ def bubbleSort(array: list[int]) -> None:
   i: int = 0
   for i in range(len(array)):
     swapped: bool = False
+    j: int = 0
     for j in range(0, len(array) - i - 1):
       if array[j] > array[j + 1]:
         temp: int = array[j]
