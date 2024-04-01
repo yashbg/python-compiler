ddb080e8b80654b3010f4eaab03c10aeb0d46816
Yash Gupta <yashbg7@gmail.com>
2024-04-01 23:55:32 +0530

    Update test2.py

--- milestone2/tests/test2.py
+++ milestone2/tests/test2.py
@@ -5,17 +5,8 @@ class A:
     self.y: float = 3.14
 
 
-class B(A):
-
-  def __init__(self):
-    self.x = 1
-    self.y = 3.14
-    self.z: str = "I am new"
-
-
 def main():
   a: A = A()
-  b: B = B()
 
 
 if __name__ == '__main__':
