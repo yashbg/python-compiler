6dfdd96ba336a17134828066164ab99727faae0b
Yash Gupta <yashbg7@gmail.com>
2024-04-05 05:47:26 +0530

    Update testcase

--- milestone2/tests/test2.py
+++ milestone2/tests/test2.py
@@ -3,9 +3,6 @@ class A:
   def __init__(self):
     self.x: int = 1
     self.y: float = 3.14
-  
-  def foo(self) -> int:
-    return 1
 
 
 class B(A):
@@ -19,7 +16,6 @@ class B(A):
 def main():
   a: A = A()
   b: B = B()
-  i: int = b.foo()
 
 
 if __name__ == '__main__':
