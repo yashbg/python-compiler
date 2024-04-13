3e3029f74bbfed34b62d7e61f78a34d576a61417
Yash Gupta <yashbg7@gmail.com>
2024-04-14 04:10:33 +0530

    Store function arguments

--- milestone3/src/codegen.cpp
+++ milestone3/src/codegen.cpp
@@ -25,76 +25,105 @@ void gen_x86_line_code(const std::vector<std::string> &ac3_line) {
   if (op == "=") {
     if (arg1 == "popparam") {
       // result = popparam
+      return;
     }
-    else {
-      // result = arg1
-      std::string arg1_addr = get_addr(arg1);
-      std::string result_addr = get_addr(result);
 
-      x86_code.push_back("\tmovl\t" + arg1_addr + ", " + "%eax");
-      x86_code.push_back("\tmovl\t%eax, " + result_addr);
-    }
+    // result = arg1
+    std::string arg1_addr = get_addr(arg1);
+    std::string result_addr = get_addr(result);
+
+    x86_code.push_back("\tmovl\t" + arg1_addr + ", " + "%eax");
+    x86_code.push_back("\tmovl\t%eax, " + result_addr);
+    return;
   }
-  else if (result == "goto") {
+
+  if (result == "goto") {
     // goto arg1
     x86_code.push_back("\tjmp\t" + arg1);
+    return;
   }
-  else if (arg1 == "goto") {
+
+  if (arg1 == "goto") {
     // result op goto arg2
+    return;
   }
-  else if (arg1 == ":" && op.empty()) {
+  
+  if (arg1 == ":" && op.empty()) {
     // result:
     cur_label = result;
 
     x86_code.push_back(result + ":");
+    return;
   }
-  else if (result == "beginfunc") {
+  
+  if (result == "beginfunc") {
     // beginfunc
     func_scope = true;
-    cur_func_symtable_ptr = lookup_func(cur_label);
+    std::string func_name = cur_label;
+    cur_func_symtable_ptr = lookup_func(func_name);
 
     x86_code.push_back("\tpushq\t%rbp");
     x86_code.push_back("\tmovq\t%rsp, %rbp");
 
     int offset = align_offset(cur_func_symtable_ptr->offset);
     x86_code.push_back("\tsubq\t$" + std::to_string(offset) + ", %rsp");
+
+    store_args(func_name);
+    return;
   }
-  else if (result == "endfunc") {
+  
+  if (result == "endfunc") {
     // endfunc
     func_scope = false;
+    return;
   }
-  else if (op == "push") {
+  
+  if (op == "push") {
     // push arg1
     std::string arg1_addr = get_addr(arg1);
 
     x86_code.push_back("\tmovl\t" + arg1_addr + ", " + "%eax");
+    return;
   }
-  else if (op == "return") {
+  
+  if (op == "return") {
     // return
     x86_code.push_back("\tleave");
     x86_code.push_back("\tret");
+    return;
   }
-  else if (op.substr(0, 8) == "popparam") {
+  
+  if (op.substr(0, 8) == "popparam") {
     // result = popparam
+    return;
   }
-  else if (op == "stackpointer") {
+  
+  if (op == "stackpointer") {
     // stackpointer arg1
+    return;
   }
-  else if (op.substr(0, 5) == "param") {
+  
+  if (op.substr(0, 5) == "param") {
     // param arg1
+    return;
   }
-  else if (result == "call") {
+  
+  if (result == "call") {
     // call arg1 arg2 op
+    return;
   }
-  else if (op.empty() && arg2.empty()) {
+  
+  if (op.empty() && arg2.empty()) {
     // result = arg1
+    return;
   }
-  else if (arg2.empty()) {
+  
+  if (arg2.empty()) {
     // result = op arg1
+    return;
   }
-  else {
-    // result = arg1 op arg2
-  }
+
+  // result = arg1 op arg2
 }
 
 std::string get_addr(const std::string &name) {
@@ -114,4 +143,14 @@ int align_offset(int offset) {
   }
 
   return offset;
+}
+
+void store_args(const std::string &func_name) {
+  local_symtable *func_symtable_ptr = lookup_func(func_name);
+  int num_args = func_symtable_ptr->params.size();
+  std::vector<std::string> regs = {"%edi", "%esi", "%edx", "%ecx", "%r8d", "%r9d"};
+  for (int i = 0; i < num_args && i < regs.size(); i++) {
+    std::string param = func_symtable_ptr->params[i].first;
+    x86_code.push_back("\tmovl\t" + regs[i] + ", " + get_addr(param));
+  }
 }
