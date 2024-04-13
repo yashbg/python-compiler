9be4859e687e6e2186ce0d383f66ac7b821e4a85
Yash Gupta <yashbg7@gmail.com>
2024-04-13 06:34:16 +0530

    Add cases in gen_x86_line_code()

--- milestone3/src/codegen.cpp
+++ milestone3/src/codegen.cpp
@@ -17,4 +17,47 @@ void gen_x86_line_code(const std::vector<std::string> &ac3_line) {
   std::string arg1 = ac3_line[1];
   std::string arg2 = ac3_line[2];
   std::string result = ac3_line[3];
+
+  if (op == "=") {
+    // result = arg1
+  }
+  else if (result == "goto") {
+    // goto arg1
+  }
+  else if (arg1 == "goto") {
+    // result op goto arg2
+  }
+  else if (arg1 == ":" && op.empty()) {
+    // result:
+  }
+  else if (result == "beginfunc" || result == "endfunc") {
+    // beginfunc | endfunc
+  }
+  else if (op == "push") {
+    // push arg1
+  }
+  else if (op == "return") {
+    // return
+  }
+  else if (op.substr(0, 8) == "popparam") {
+    // result = popparam
+  }
+  else if (op == "stackpointer") {
+    // stackpointer arg1
+  }
+  else if (op.substr(0, 5) == "param") {
+    // param arg1
+  }
+  else if (result == "call") {
+    // call arg1 arg2 op
+  }
+  else if (op.empty() && arg2.empty()) {
+    // result = arg1
+  }
+  else if (arg2.empty()) {
+    // result = op arg1
+  }
+  else {
+    // result = arg1 op arg2
+  }
 }
