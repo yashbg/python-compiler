4e89939c7031df6a90124fd36aca65bbd7136ffc
Shivang Pandey <shivangp20@iitk.ac.in>
2024-04-18 00:02:21 +0530

    update test3

--- milestone3/tests/test3.py
+++ milestone3/tests/test3.py
@@ -6,18 +6,28 @@ def main():
   s: str = "Hello, World!"
   foo(s)
 
-  s1: str = "abc"
-  s2: str = "abcdef"
+  s1: str = "cde"
+  s2: str = "abc"
+
+  print("String 1:")
   foo(s1)
+
+  print("String 2:")
   foo(s2)
+  
+  print("== != check:")
   if s1 == s2:
     foo("Strings are equal")
   elif s1 != s2:
     foo("Strings are not equal")
+  
+  print("< > check:")
   if s1 < s2:
     foo("String 1 is less than String 2")
   elif s1 > s2:
     foo("String 1 is greater than String 2")
+  
+  print("<= >= check:")
   if s1 <= s2:
     foo("String 1 is less than or equal to String 2") 
   elif s1 >= s2:  
